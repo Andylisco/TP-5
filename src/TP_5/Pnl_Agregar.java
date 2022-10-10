47cead04ba9eed1c0f1a31139ed1edc3cb33d6c7
Tomas Cañón <72145064+TomasNiCanon@users.noreply.github.com>
2022-10-09 23:34:51 -0300

    ya se ve el combobox

--- src/TP_5/Pnl_Agregar.java
+++ src/TP_5/Pnl_Agregar.java
@@ -36,33 +36,25 @@ public class Pnl_Agregar extends JPanel {
 	 * Create the panel.
 	 */
 	public Pnl_Agregar() {
-		SpringLayout springLayout = new SpringLayout();
-		setLayout(springLayout);
+		setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("ID");
-		springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel, 17, SpringLayout.NORTH, this);
-		springLayout.putConstraint(SpringLayout.WEST, lblNewLabel, 64, SpringLayout.WEST, this);
+		lblNewLabel.setBounds(64, 17, 11, 14);
 		add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("");
-		springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel_1, 0, SpringLayout.NORTH, lblNewLabel);
-		springLayout.putConstraint(SpringLayout.WEST, lblNewLabel_1, 77, SpringLayout.EAST, lblNewLabel);
+		lblNewLabel_1.setBounds(152, 17, 35, 14);
 		add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("Nombre");
-		springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel_2, 27, SpringLayout.SOUTH, lblNewLabel);
-		springLayout.putConstraint(SpringLayout.WEST, lblNewLabel_2, 64, SpringLayout.WEST, this);
-		springLayout.putConstraint(SpringLayout.EAST, lblNewLabel_2, 110, SpringLayout.WEST, this);
+		lblNewLabel_2.setBounds(64, 58, 46, 21);
 		add(lblNewLabel_2);
 		
 
 
-		@SuppressWarnings("rawtypes")
-		JComboBox<Generos> cb_Genero = new JComboBox<Generos>();
-		springLayout.putConstraint(SpringLayout.WEST, cb_Genero, 153, SpringLayout.WEST, this);
-		springLayout.putConstraint(SpringLayout.SOUTH, cb_Genero, -168, SpringLayout.SOUTH, this);
-		springLayout.putConstraint(SpringLayout.EAST, cb_Genero, 243, SpringLayout.WEST, this);
-		springLayout.putConstraint(SpringLayout.EAST, cb_Genero, 348, SpringLayout.WEST, this);
+		//@SuppressWarnings("rawtypes")
+		cb_Genero = new JComboBox<Generos>();
+		cb_Genero.setBounds(153, 111, 195, 21);
 		cb_Genero.addItem(new Generos("Seleccione un Genero"));
 		cb_Genero.addItem(new Generos("Terror"));
 		cb_Genero.addItem(new Generos("Accion"));
@@ -72,16 +64,13 @@ public class Pnl_Agregar extends JPanel {
 		
 		
 		txtNombre = new JTextField();
-		springLayout.putConstraint(SpringLayout.NORTH, cb_Genero, 32, SpringLayout.SOUTH, txtNombre);
-		springLayout.putConstraint(SpringLayout.NORTH, txtNombre, 0, SpringLayout.NORTH, lblNewLabel_2);
-		springLayout.putConstraint(SpringLayout.WEST, txtNombre, 0, SpringLayout.WEST, lblNewLabel_1);
-		springLayout.putConstraint(SpringLayout.SOUTH, txtNombre, 79, SpringLayout.NORTH, this);
-		springLayout.putConstraint(SpringLayout.EAST, txtNombre, 128, SpringLayout.EAST, lblNewLabel_2);
+		txtNombre.setBounds(152, 58, 86, 21);
 		add(txtNombre);
 		txtNombre.setColumns(10);
 		Peliculas p = new Peliculas();
 		lblNewLabel_1.setText(p.getId());
 		JButton btnNewButton = new JButton("Aceptar");
+		btnNewButton.setBounds(64, 164, 115, 37);
 		ArrayList<Peliculas> ListaPeliculas = new ArrayList<Peliculas>();
 		DlModel =new DefaultListModel<ArrayList>();
 		btnNewButton.addActionListener(new ActionListener() {
@@ -106,16 +95,10 @@ public class Pnl_Agregar extends JPanel {
 
 			
 		});
-		springLayout.putConstraint(SpringLayout.WEST, btnNewButton, 0, SpringLayout.WEST, lblNewLabel);
 		add(btnNewButton);
 		
 		JLabel lblNewLabel_3 = new JLabel("Genero");
-		springLayout.putConstraint(SpringLayout.NORTH, btnNewButton, 39, SpringLayout.SOUTH, lblNewLabel_3);
-		springLayout.putConstraint(SpringLayout.SOUTH, btnNewButton, 73, SpringLayout.SOUTH, lblNewLabel_3);
-		springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel_3, 111, SpringLayout.NORTH, this);
-		springLayout.putConstraint(SpringLayout.SOUTH, lblNewLabel_2, -32, SpringLayout.NORTH, lblNewLabel_3);
-		springLayout.putConstraint(SpringLayout.WEST, lblNewLabel_3, 64, SpringLayout.WEST, this);
-		springLayout.putConstraint(SpringLayout.EAST, lblNewLabel_3, 110, SpringLayout.WEST, this);
+		lblNewLabel_3.setBounds(64, 111, 46, 14);
 		add(lblNewLabel_3);
 		
 		
--- src/TP_5/Pnl_Listar.java
+++ src/TP_5/Pnl_Listar.java
@@ -13,24 +13,30 @@ public class Pnl_Listar extends JPanel {
 	/**
 	 * Create the panel.
 	 */
-	
+	private JPanel contentPanel;
 	private JLabel  lblPeliculas;
 	private JList JlistPeliculas;
 	private DefaultListModel<ArrayList> DlModel;
 	
 	
 	public Pnl_Listar() {
-		setLayout(new BorderLayout(0, 0));
+		setLayout(null);
 		
 		lblPeliculas = new JLabel("Peliculas");
-		add(lblPeliculas, BorderLayout.WEST);
+		lblPeliculas.setBounds(0, 0, 40, 300);
+		add(lblPeliculas);
 		
-		JlistPeliculas = new JList<ArrayList>();
-		add(JlistPeliculas, BorderLayout.CENTER);
+		JlistPeliculas = new JList();
+		JlistPeliculas.setBounds(40, 0, 410, 300);
+		
+		
+		
 		
 		DlModel= new DefaultListModel<ArrayList>();
 		
 		JlistPeliculas.setModel(DlModel);
+		
+		add(JlistPeliculas);
 		
 		
 
